4089de98df3e90574ce975b0428fb4ea4be5195d
natthakit111 <natthakit_ya66@live.ac.th>
2023-10-11 20:16:25 +0700

    update hw12-1

--- Homework/HW12/hw12-1-basic-structure.cpp
+++ Homework/HW12/hw12-1-basic-structure.cpp
@@ -40,3 +40,50 @@
         Score : 87.60 points
 
 */
+#include <stdio.h>
+
+struct Person {
+    char Name[ 20 ] ;
+    int Age ;
+    char Sex ;
+    float Score ;
+} ;
+
+void printPersonInfo( struct Person person, int personNumber ) {
+    printf( "--| Person %c Information |--\n", 'A' + personNumber ) ;
+    printf( "Name: %s (%c)\n", person.Name, person.Sex ) ;
+    printf( "Age: %d years old\n", person.Age ) ;
+    printf( "Score: %.2f points\n", person.Score ) ;
+}//end void
+
+int main() {
+    struct Person A, B ;
+
+    
+    printf( "Person A\n" ) ;
+    printf( "Name: " ) ;
+    scanf( "%s", A.Name ) ;
+    printf( "Age: " ) ;
+    scanf( "%d", &A.Age ) ;
+    printf( "Sex (M/F): " ) ;
+    scanf( " %c", &A.Sex ) ;  
+    printf( "Score: " ) ;
+    scanf( "%f", &A.Score ) ;
+
+    
+    printf( "\nPerson B\n" ) ;
+    printf( "Name: " ) ;
+    scanf( "%s", B.Name ) ;
+    printf( "Age: " ) ;
+    scanf( "%d", &B.Age ) ;
+    printf( "Sex (M/F): " ) ;
+    scanf( " %c", &B.Sex ) ;
+    printf( "Score: " ) ;
+    scanf( "%f", &B.Score );
+
+    printf( "\nOutput:\n" ) ;
+    printPersonInfo( A, 0 ) ;
+    printPersonInfo( B, 1 ) ;
+
+    return 0 ;
+}//end function main
